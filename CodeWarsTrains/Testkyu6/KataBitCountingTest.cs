fd2160eb504d8b14df02a06d798fcf0200b5a155
zhoubo56 <zhoubo56@126.com>
2019-04-01 14:50:47 +0800

    完成6级的Bit Counting

---
+++ CodeWarsTrains/Testkyu6/KataBitCountingTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using kyu6;
+
+namespace Testkyu6
+{
+    [TestClass]
+    public class KataBitCountingTest
+    {
+        [TestMethod]
+        public void TestCountBits()
+        {
+            Assert.AreEqual(0, KataBitCounting.CountBits(0));
+            Assert.AreEqual(1, KataBitCounting.CountBits(4));
+            Assert.AreEqual(3, KataBitCounting.CountBits(7));
+            Assert.AreEqual(2, KataBitCounting.CountBits(9));
+            Assert.AreEqual(2, KataBitCounting.CountBits(10));
+        }
+    }
+}
---
+++ CodeWarsTrains/kyu6/KataBitCounting.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kyu6
+{
+    /// <summary>
+    /// Bit Counting
+    /// https://www.codewars.com/kata/526571aae218b8ee490006f4
+    /// </summary>
+    public class KataBitCounting
+    {
+        public static int CountBits(int n)
+        {
+            int num = 0;
+            do
+            {
+                if (n % 2 == 1) num++;
+                n = n / 2;
+            } while (n != 0);
+            return num;
+
+            // Best Practices
+            //return Convert.ToString(n, 2).Count(x => x == '1');
+        }
+    }
+}
