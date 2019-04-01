358f4180e64ccc4ab35caf70d72756bacd9693d3
zhoubo56 <zhoubo56@126.com>
2019-04-01 15:58:20 +0800

    完成6级的Find the unique number

---
+++ CodeWarsTrains/Testkyu6/KataFindUniqueNumberTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using kyu6;
+
+namespace Testkyu6
+{
+    /// <summary>
+    /// Summary description for KataFindUniqueNumberTest
+    /// </summary>
+    [TestClass]
+    public class KataFindUniqueNumberTest
+    {
+        [TestMethod]
+        public void TestGetUnique()
+        {
+            Assert.AreEqual(1, KataFindUniqueNumber.GetUnique(new[] { 1, 2, 2, 2 }));
+            Assert.AreEqual(-2, KataFindUniqueNumber.GetUnique(new[] { -2, 2, 2, 2 }));
+            Assert.AreEqual(14, KataFindUniqueNumber.GetUnique(new[] { 11, 11, 14, 11, 11 }));
+        }
+    }
+}
---
+++ CodeWarsTrains/kyu6/KataFindUniqueNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kyu6
+{
+    /// <summary>
+    /// Find the unique number
+    /// https://www.codewars.com/kata/585d7d5adb20cf33cb000235
+    /// </summary>
+    public class KataFindUniqueNumber
+    {
+        public static int GetUnique(IEnumerable<int> numbers)
+        {
+            var tmp = numbers.OrderBy(n => n).ToList();
+            if (tmp[0] == tmp[1])
+            {
+                tmp = numbers.OrderByDescending(n => n).ToList();
+            }
+
+            return tmp.FirstOrDefault();
+
+            //return numbers.GroupBy(x => x).Single(x => x.Count() == 1).Key;
+        }
+    }
+}
