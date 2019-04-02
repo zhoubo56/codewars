434350ab450a06fb43ea72f48fd16a189490181b
zhoubo56 <zhoubo56@126.com>
2019-04-02 13:57:17 +0800

    完成5级的Human Readable Time

---
+++ CodeWarsTrains/Testkyu5/KataHumanReadableTimeTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using kyu5;
+
+namespace Testkyu5
+{
+    /// <summary>
+    /// Summary description for KataHumanReadableTimeTest
+    /// </summary>
+    [TestClass]
+    public class KataHumanReadableTimeTest
+    {
+        [TestMethod]
+        public void TestGetReadableTime()
+        {
+            Assert.AreEqual("00:00:00", KataHumanReadableTime.GetReadableTime(0));
+            Assert.AreEqual("00:00:05", KataHumanReadableTime.GetReadableTime(5));
+            Assert.AreEqual("00:01:00", KataHumanReadableTime.GetReadableTime(60));
+            Assert.AreEqual("23:59:59", KataHumanReadableTime.GetReadableTime(86399));
+            Assert.AreEqual("99:59:59", KataHumanReadableTime.GetReadableTime(359999));
+        }
+    }
+}
---
+++ CodeWarsTrains/kyu5/KataHumanReadableTime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kyu5
+{
+    /// <summary>
+    /// Human Readable Time
+    /// https://www.codewars.com/kata/52685f7382004e774f0001f7
+    /// </summary>
+    public class KataHumanReadableTime
+    {
+        public static string GetReadableTime(int seconds)
+        {
+            int hour = seconds / 3600;
+            seconds = seconds - (hour * 3600);
+            int minute = seconds / 60;
+            seconds = seconds - (minute * 60);
+            return $"{hour.ToString("00")}:{minute.ToString("00")}:{seconds.ToString("00")}";
+
+            //return $"{seconds / 3600:00}:{seconds / 60 % 60:00}:{seconds % 60:00}";
+        }
+    }
+}
