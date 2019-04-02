528591f5bc7f0e8307d202dbae0885b38689e436
zhoubo56 <zhoubo56@126.com>
2019-04-02 11:06:51 +0800

    完成5级的Number of trailing zeros of N!

---
+++ CodeWarsTrains/Testkyu5/KataNumberOfTrailingZerosTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using kyu5;
+
+namespace Testkyu5
+{
+    /// <summary>
+    /// Summary description for KataNumberOfTrailingZerosTest
+    /// </summary>
+    [TestClass]
+    public class KataNumberOfTrailingZerosTest
+    {
+        [TestMethod]
+        public void TestTrailingZeros()
+        {
+            Assert.AreEqual(1, KataNumberOfTrailingZeros.TrailingZeros(5));
+            Assert.AreEqual(6, KataNumberOfTrailingZeros.TrailingZeros(25));
+            Assert.AreEqual(131, KataNumberOfTrailingZeros.TrailingZeros(531));
+        }
+    }
+}
---
+++ CodeWarsTrains/kyu5/KataNumberOfTrailingZeros.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kyu5
+{
+    /// <summary>
+    /// Number of trailing zeros of N!
+    /// https://www.codewars.com/kata/52f787eb172a8b4ae1000a34
+    /// </summary>
+    public class KataNumberOfTrailingZeros
+    {
+        public static int TrailingZeros(int n)
+        {
+            //思路没问题，但是阶乘中2个因子数肯定大于5的，所以只需要计算5的因子数
+            var two = solve(n, 2);
+            var five = solve(n, 5);
+            return two < five ? two : five;
+        }
+
+        private static int solve(int n, int x)
+        {
+            int cnt = 0;
+            while (n > 0) cnt += (n /= x);
+            return cnt;
+        }
+    }
+}
