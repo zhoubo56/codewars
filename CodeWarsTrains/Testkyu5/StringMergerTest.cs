cb4f0efb4a0009d73b2cda4206202d91477e759e
zhoubo56 <zhoubo56@126.com>
2019-04-17 15:11:59 +0800

    完成5级的Merged String Checker

---
+++ CodeWarsTrains/Testkyu5/StringMergerTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using kyu5;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testkyu5
+{
+    /// <summary>
+    /// Summary description for StringMergerTest
+    /// </summary>
+    [TestClass]
+    public class StringMergerTest
+    {
+        [TestMethod]
+        public void HappyPath1()
+        {
+            Assert.IsTrue(StringMerger.isMerge("codewars", "code", "wars"), "codewars can be created from code and wars");
+        }
+
+        [TestMethod]
+        public void HappyPath2()
+        {
+            Assert.IsTrue(StringMerger.isMerge("codewars", "cdwr", "oeas"), "codewars can be created from cdwr and oeas");
+        }
+
+        [TestMethod]
+        public void SadPath1()
+        {
+            Assert.IsFalse(StringMerger.isMerge("codewars", "cod", "wars"), "Codewars are not codwars");
+        }
+
+        [TestMethod]
+        public void CanHandleBananas()
+        {
+            Assert.IsTrue(StringMerger.isMerge("bananas", "baa", "anns"), "Going bananas!");
+        }
+
+        [TestMethod]
+        public void HappyPath4()
+        {
+            Assert.IsTrue(StringMerger.isMerge("Can we merge it? Yes, we can!", "n ee tYw n!", "Cawe mrgi? es, eca"), "Can we merge it? Yes, we can!' is a merge of 'n ee tYw n!' and 'Cawe mrgi? es, eca");
+        }
+    }
+}
---
+++ CodeWarsTrains/kyu5/StringMerger.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace kyu5
+{
+    /// <summary>
+    /// Merged String Checker
+    /// https://www.codewars.com/kata/merged-string-checker
+    /// </summary>
+    public class StringMerger
+    {
+        public static bool isMerge(string s, string part1, string part2)
+        {
+            if (s.Length != part1.Length + part2.Length)
+            {
+                return false;
+            }
+
+            int index = 0, index1 = 0, index2 = 0;
+            var snapshot = new Stack<int[]>();
+            while (index < s.Length)
+            {
+                if (index1 < part1.Length && s[index] == part1[index1])
+                {
+                    if (index2 < part2.Length && part1[index1] == part2[index2])
+                    {
+                        snapshot.Push(new int[] { index + 1, index1, index2 + 1 });
+                    }
+                    index1++;
+                    index++;
+                }
+                else if (index2 < part2.Length && s[index] == part2[index2])
+                {
+                    index2++;
+                    index++;
+                }
+                else
+                {
+                    if (snapshot.Count > 0)
+                    {
+                        var tmp = snapshot.Pop();
+                        index = tmp[0];
+                        index1 = tmp[1];
+                        index2 = tmp[2];
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
