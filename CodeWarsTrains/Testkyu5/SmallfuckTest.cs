c35fdf0bb215d8e3d21eb0af4f96d249acc3ef8a
zhoubo56 <zhoubo56@126.com>
2019-04-18 11:06:44 +0800

    完成5级的Esolang Interpreters #2 - Custom Smallfuck Interpreter

---
+++ CodeWarsTrains/Testkyu5/SmallfuckTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using kyu5;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testkyu5
+{
+    /// <summary>
+    /// Summary description for SmallfuckTest
+    /// </summary>
+    [TestClass]
+    public class SmallfuckTest
+    {
+        [TestMethod, Description("should work for some example test cases")]
+        public void ExampleTest()
+        {
+            // Flips the leftmost cell of the tape
+            Assert.AreEqual("10101100", Smallfuck.Interpreter("*", "00101100"));
+            // Flips the second and third cell of the tape
+            Assert.AreEqual("01001100", Smallfuck.Interpreter(">*>*", "00101100"));
+            // Flips all the bits in the tape
+            Assert.AreEqual("11010011", Smallfuck.Interpreter("*>*>*>*>*>*>*>*", "00101100"));
+            // Flips all the bits that are initialized to 0
+            Assert.AreEqual("11111111", Smallfuck.Interpreter("*>*>>*>>>*>*", "00101100"));
+            // Goes somewhere to the right of the tape and then flips all bits that are initialized to 1, progressing leftwards through the tape
+            Assert.AreEqual("00000000", Smallfuck.Interpreter(">>>>>*<*<<*", "00101100"));
+        }
+
+        [TestMethod, Description("should return the final state of the tape immediately if the pointer ever goes out of bounds")]
+        public void OutOfBoundsTest()
+        {
+            Assert.AreEqual("1001101000000111", Smallfuck.Interpreter("*>>>*>*>>*>>>>>>>*>*>*>*>>>**>>**", "0000000000000000"), "Index was outside the bounds of the array");
+        }
+
+        [TestMethod, Description("should work for some simple and nested loops")]
+        public void NestedLoopsTest()
+        {
+            Assert.AreEqual("1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111",
+                Smallfuck.Interpreter("*[>*]",
+                    $"0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"), "Your interpreter should evaluate a simple non-nested loop properly String lengths are both 256.Strings differ at index 2");
+
+            Assert.AreEqual("0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+                Smallfuck.Interpreter("[>*]",
+                    $"0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"), "Your interpreter should jump to the matching \"]\" when it encounters a \"[\" and the bit under the pointer is 0 String lengths are both 256.Strings differ at index 1.");
+
+            Assert.AreEqual("000", Smallfuck.Interpreter("[[]*>*>*>]", "000"), "Your interpreter should also work properly with nested loops String lengths are both 3.Strings differ at index 0.");
+        }
+    }
+}
---
+++ CodeWarsTrains/kyu5/Smallfuck.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace kyu5
+{
+    /// <summary>
+    /// Esolang Interpreters #2 - Custom Smallfuck Interpreter
+    /// https://www.codewars.com/kata/esolang-interpreters-number-2-custom-smallfuck-interpreter
+    /// </summary>
+    public class Smallfuck
+    {
+        public static string Interpreter(string code, string tape)
+        {
+            //Console.WriteLine($"{code} | {tape}");
+            int flag = 0, index = 0;
+            var dicBracket = new Dictionary<int, bool>();
+            var result = tape.Select((c) => c == '1').ToArray();
+            try
+            {
+                while (index < code.Length && flag < result.Length)
+                {
+                    switch (code[index])
+                    {
+                        case '>':
+                            if (!dicBracket.Any(d => d.Value)) flag += 1;
+                            break;
+                        case '<':
+                            if (!dicBracket.Any(d => d.Value)) flag -= 1;
+                            break;
+                        case '*':
+                            if (!dicBracket.Any(d => d.Value)) result[flag] = !result[flag];
+                            break;
+                        case '[':
+                            dicBracket.Add(index, !result[flag]);
+                            break;
+                        case ']':
+                            var last = dicBracket.LastOrDefault();
+                            if (last.Value)
+                            {
+                                dicBracket.Remove(last.Key);
+                            }
+                            else
+                            {
+                                if (result[flag])
+                                {
+                                    index = last.Key;
+                                }
+                                else
+                                {
+                                    dicBracket.Remove(last.Key);
+                                }
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                    index++;
+                }
+            }
+            catch (Exception ex)
+            {
+                //should return immediately 
+            }
+
+            return string.Join("", result.Select((c) => c ? '1' : '0'));
+        }
+
+        public static string InterpreterBak(string code, string tape)
+        {
+            Console.WriteLine($"{code} | {tape}");
+            int flag = 0, index = 0, leftBracket = -1;
+            var past = false;
+            var result = tape.Select((c) => c == '1').ToArray();
+            try
+            {
+                while (index < code.Length)
+                {
+                    switch (code[index])
+                    {
+                        case '>':
+                            if (!past) flag += 1;
+                            break;
+                        case '<':
+                            if (!past) flag -= 1;
+                            break;
+                        case '*':
+                            if (!past) result[flag] = !result[flag];
+                            break;
+                        case '[':
+                            if (result[flag])
+                            {
+                                if (leftBracket == -1)
+                                {
+                                    leftBracket = index;
+                                }
+                            }
+                            else
+                            {
+                                past = true;
+                            }
+                            break;
+                        case ']':
+                            past = false;
+                            if (result[flag])
+                            {
+                                if (leftBracket > -1) index = leftBracket;
+                            }
+                            else
+                            {
+                                leftBracket = -1;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                    index++;
+                }
+            }
+            catch (Exception ex)
+            {
+                //should return immediately 
+            }
+
+            return string.Join("", result.Select((c) => c ? '1' : '0'));
+        }
+    }
+}
