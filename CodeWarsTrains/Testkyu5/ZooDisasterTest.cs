23378ba0130c7da6fc34e5a2bef267e204153145
zhoubo56 <zhoubo56@126.com>
2019-04-18 13:09:40 +0800

    完成5级的The Hunger Games - Zoo Disaster!

---
+++ CodeWarsTrains/Testkyu5/ZooDisasterTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using kyu5;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testkyu5
+{
+    /// <summary>
+    /// Summary description for ZooDisasterTest
+    /// </summary>
+    [TestClass]
+    public class ZooDisasterTest
+    {
+        [TestMethod]
+        public void Example()
+        {
+            string input = "fox,bug,chicken,grass,sheep";
+            string[] expected = {
+                "fox,bug,chicken,grass,sheep",
+                "chicken eats bug",
+                "fox eats chicken",
+                "sheep eats grass",
+                "fox eats sheep",
+                "fox"
+            };
+
+            CollectionAssert.AreEqual(expected, ZooDisaster.WhoEatsWho(input));
+        }
+
+        [TestMethod]
+        public void EmpytyZoo()
+        {
+            string input = "";
+            string[] expected = {
+                "",
+            };
+
+            CollectionAssert.AreEqual(expected, ZooDisaster.WhoEatsWho(input));
+        }
+
+        [TestMethod]
+        public void SingleKnownThing()
+        {
+            string input = "bug";
+            string[] expected = {
+                "bug",
+                "bug"
+            };
+
+            CollectionAssert.AreEqual(expected, ZooDisaster.WhoEatsWho(input));
+        }
+    }
+}
---
+++ CodeWarsTrains/kyu5/ZooDisaster.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kyu5
+{
+    /// <summary>
+    /// The Hunger Games - Zoo Disaster!
+    /// https://www.codewars.com/kata/the-hunger-games-zoo-disaster
+    /// </summary>
+    public class ZooDisaster
+    {
+        private static readonly Dictionary<string, string> eatList = new Dictionary<string, string>()
+        {
+            {"antelope","|grass|"},
+            {"big-fish","|little-fish|"},
+            {"bug","|leaves|"},
+            {"bear","|big-fish|bug|chicken|cow|leaves|sheep|"},
+            {"chicken","|bug|"},
+            {"cow","|grass|"},
+            {"fox","|chicken|sheep|"},
+            {"giraffe","|leaves|"},
+            {"lion","|antelope|cow|"},
+            {"panda","|leaves|"},
+            {"sheep","|grass|"},
+        };
+
+        public static string[] WhoEatsWho(string zoo)
+        {
+            if (string.IsNullOrEmpty(zoo)) return new[] { "" };
+            var animals = zoo.Split(',').ToList();
+            var result = new List<string>() { zoo };
+            var canEat = true;
+            while (canEat)
+            {
+                for (var i = 0; i < animals.Count; i++)
+                {
+                    if (eatList.ContainsKey(animals[i]))
+                    {
+                        if (i > 0 && eatList[animals[i]].Contains($"|{animals[i - 1]}|"))
+                        {
+                            result.Add($"{animals[i]} eats {animals[i - 1]}");
+                            animals.RemoveAt(i - 1);
+                            break;
+                        }
+
+                        if (i + 1 < animals.Count && eatList[animals[i]].Contains($"|{animals[i + 1]}|"))
+                        {
+                            result.Add($"{animals[i]} eats {animals[i + 1]}");
+                            animals.RemoveAt(i + 1);
+                            break;
+                        }
+                    }
+
+                    if (i == animals.Count - 1)
+                    {
+                        canEat = false;
+                    }
+                }
+            }
+            result.Add(string.Join(",", animals.Select((a) => a)));
+            return result.ToArray();
+        }
+    }
+}
