a768da11eee37f06cd82df21f1fa6c5352cabdb8
zhoubo56 <zhoubo56@126.com>
2019-04-19 12:58:09 +0800

    完成4级的Strings Mix

---
+++ CodeWarsTrains/Testkyu4/StringsMixTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using kyu4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testkyu4
+{
+    /// <summary>
+    /// Summary description for StringsMixTest
+    /// </summary>
+    [TestClass]
+    public class StringsMixTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Assert.AreEqual("2:eeeee/2:yy/=:hh/=:rr", StringsMix.Mix("Are they here", "yes, they are here"));
+            Assert.AreEqual("1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg",
+                StringsMix.Mix("looping is fun but dangerous", "less dangerous than coding"));
+            Assert.AreEqual("1:aaa/1:nnn/1:gg/2:ee/2:ff/2:ii/2:oo/2:rr/2:ss/2:tt",
+                StringsMix.Mix(" In many languages", " there's a pair of functions"));
+            Assert.AreEqual("1:ee/1:ll/1:oo", StringsMix.Mix("Lords of the Fallen", "gamekult"));
+            Assert.AreEqual("", StringsMix.Mix("codewars", "codewars"));
+            Assert.AreEqual("1:nnnnn/1:ooooo/1:tttt/1:eee/1:gg/1:ii/1:mm/=:rr",
+                StringsMix.Mix("A generation must confront the looming ", "codewarrs"));
+        }
+    }
+}
---
+++ CodeWarsTrains/kyu4/StringsMix.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace kyu4
+{
+    /// <summary>
+    /// Strings Mix
+    /// https://www.codewars.com/kata/strings-mix 
+    /// </summary>
+    public class StringsMix
+    {
+        private const int StartLetter = 97;
+        private const int LetterCount = 26;
+        private const char Separate = '/';
+
+        public static string Mix(string s1, string s2)
+        {
+            var s1MixObjects = new List<MixObject>();
+            foreach (var c1 in s1)
+            {
+                CalculateTimes(c1, 1, ref s1MixObjects);
+            }
+
+            var s2MixObjects = new List<MixObject>();
+            foreach (var c2 in s2)
+            {
+                CalculateTimes(c2, 2, ref s2MixObjects);
+            }
+
+            var allMixObjects = s1MixObjects.Union(s2MixObjects).Where(m => m.Count > 1).ToList();
+            foreach (var group in allMixObjects.GroupBy(m => m.Letter))
+            {
+                var mixObjects = group.ToList();
+                if (mixObjects.Count < 2) continue;
+                if (mixObjects[0].Count == mixObjects[1].Count)
+                {
+                    allMixObjects.Remove(mixObjects[1]);
+                    allMixObjects.Remove(mixObjects[0]);
+                    allMixObjects.Add(new MixObject(mixObjects[0]));
+                }
+                else if (mixObjects[0].Count > mixObjects[1].Count)
+                {
+                    allMixObjects.Remove(mixObjects[1]);
+                }
+                else
+                {
+                    allMixObjects.Remove(mixObjects[0]);
+                }
+            }
+
+            var result = allMixObjects.OrderByDescending(m => m.Count).ThenBy(m => m.Belong).ThenBy(m => m.Letter).Aggregate(string.Empty, (current, mixObject) => current + (mixObject + Separate.ToString()));
+
+            return result.TrimEnd(Separate);
+        }
+
+        private static void CalculateTimes(char c, int belong, ref List<MixObject> list)
+        {
+            if (list.Any(m => m.Letter == c))
+            {
+                var mixObject = list.FirstOrDefault(m => m.Letter == c);
+                if (mixObject != null)
+                {
+                    mixObject.Count += 1;
+                }
+            }
+
+            if (c >= StartLetter && c < StartLetter + LetterCount)
+            {
+                list.Add(new MixObject(belong, c));
+            }
+        }
+    }
+
+    public class MixObject
+    {
+        public MixObject(MixObject mixObject)
+        {
+            Belong = 3;
+            Letter = mixObject.Letter;
+            Count = mixObject.Count;
+        }
+
+        public MixObject(int belong, char letter)
+        {
+            Belong = belong;
+            Letter = letter;
+            Count = 1;
+        }
+
+        public int Belong { get; set; }
+
+        public char Letter { get; set; }
+
+        public int Count { get; set; }
+
+        public override string ToString()
+        {
+            return Belong < 3 ? $"{Belong}:{new string(Letter, Count)}" : $"=:{new string(Letter, Count)}";
+        }
+    }
+}
