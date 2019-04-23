2928b442b697037bfdbb58376f2aafb8e9be598d
zhoubo56 <zhoubo56@126.com>
2019-04-23 15:46:15 +0800

    完成4级的Catching Car Mileage Numbers

---
+++ CodeWarsTrains/Testkyu4/CarMileageNumbersTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using kyu4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testkyu4
+{
+    /// <summary>
+    /// Summary description for CarMileageNumbersTest
+    /// </summary>
+    [TestClass]
+    public class CarMileageNumbersTest
+    {
+        [TestMethod]
+        public void ShouldWorkTest()
+        {
+            Assert.AreEqual(0, CarMileageNumbers.IsInteresting(3, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(1, CarMileageNumbers.IsInteresting(1336, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(2, CarMileageNumbers.IsInteresting(1337, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(0, CarMileageNumbers.IsInteresting(11208, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(1, CarMileageNumbers.IsInteresting(11209, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(2, CarMileageNumbers.IsInteresting(11211, new List<int>() { 1337, 256 }));
+        }
+
+        [TestMethod]
+        public void AdvanceTest()
+        {
+            Assert.AreEqual(1, CarMileageNumbers.IsInteresting(234567889, new List<int>() { }));
+            Assert.AreEqual(1, CarMileageNumbers.IsInteresting(98, new List<int>() { }));
+        }
+    }
+}
---
+++ CodeWarsTrains/kyu4/CarMileageNumbers.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace kyu4
+{
+    /// <summary>
+    /// Catching Car Mileage Numbers
+    /// https://www.codewars.com/kata/catching-car-mileage-numbers
+    /// </summary>
+    public class CarMileageNumbers
+    {
+        public static int IsInteresting(int number, List<int> awesomePhrases)
+        {
+            for (var i = 0; i < 3; i++)
+            {
+                var curNumber = number + i;
+                if (curNumber < 100) continue;
+
+                if (IsMatchAwesomePhrases(curNumber, awesomePhrases)) return i > 0 ? 1 : 2;
+                if (IsAllZeros(curNumber)) return i > 0 ? 1 : 2;
+                if (IsSameNumbers(curNumber, curNumber % 10)) return i > 0 ? 1 : 2;
+
+                var list = NumberToList(curNumber);
+                if (IsPalindrome(list)) return i > 0 ? 1 : 2;
+                if (IsSequential(list, -1)) return i > 0 ? 1 : 2;
+                if (list.Last() == 0) list[list.Count - 1] = 10;
+                if (IsSequential(list, 1)) return i > 0 ? 1 : 2;
+            }
+
+            return 0;
+        }
+
+        private static bool IsAllZeros(int number)
+        {
+            if (number % 10 != 0) return false;
+            number /= 10;
+            return number < 10 || IsAllZeros(number);
+        }
+
+        private static bool IsSameNumbers(int number, int last)
+        {
+            number /= 10;
+            if (number < 10) return number == last;
+
+            return number % 10 == last && IsSameNumbers(number, last);
+        }
+
+        private static bool IsSequential(List<int> list, int rate)
+        {
+            for (var i = 0; i < list.Count - 1; i++)
+            {
+                if ((list[i] + rate) != list[i + 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool IsPalindrome(List<int> list)
+        {
+            for (var i = 0; i < list.Count / 2; i++)
+            {
+                if (list[i] != list[list.Count - 1 - i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool IsMatchAwesomePhrases(int number, List<int> awesomePhrases)
+        {
+            return awesomePhrases.Contains(number);
+        }
+
+        private static List<int> NumberToList(int number)
+        {
+            var result = new List<int>();
+            while (number > 0)
+            {
+                result.Add(number % 10);
+                number /= 10;
+            }
+            result.Reverse();
+            return result;
+        }
+    }
+}
