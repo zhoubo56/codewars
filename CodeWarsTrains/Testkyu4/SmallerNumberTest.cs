35938646152c9d3296a5bda6dea49371566bf5c6
zhoubo56 <zhoubo56@126.com>
2019-06-17 17:09:53 +0800

    完成4级的Next smaller number with the same digits

---
+++ CodeWarsTrains/Testkyu4/SmallerNumberTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using kyu4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testkyu4
+{
+    [TestClass]
+    public class SmallerNumberTest
+    {
+        [TestMethod]
+        public void TestNextSmaller()
+        {
+            //SampleTests
+            Assert.AreEqual(12, SmallerNumber.NextSmaller(21));
+            Assert.AreEqual(790, SmallerNumber.NextSmaller(907));
+            Assert.AreEqual(513, SmallerNumber.NextSmaller(531));
+            Assert.AreEqual(-1, SmallerNumber.NextSmaller(1027));
+            Assert.AreEqual(414, SmallerNumber.NextSmaller(441));
+            Assert.AreEqual(123456789, SmallerNumber.NextSmaller(123456798));
+
+            //FixedTests
+            Assert.AreEqual(20990, SmallerNumber.NextSmaller(29009));
+            Assert.AreEqual(153, SmallerNumber.NextSmaller(315));
+            Assert.AreEqual(144, SmallerNumber.NextSmaller(414));
+            Assert.AreEqual(59884848459853, SmallerNumber.NextSmaller(59884848483559L));
+
+            //RandomTests
+            Assert.AreEqual(51226262627551, SmallerNumber.NextSmaller(51226262651257L));
+            Assert.AreEqual(28062, SmallerNumber.NextSmaller(28206));
+            Assert.AreEqual(850912544212556627, SmallerNumber.NextSmaller(850912544212556672));
+        }
+    }
+}
---
+++ CodeWarsTrains/kyu4/SmallerNumber.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace kyu4
+{
+    /// <summary>
+    /// Next smaller number with the same digits
+    /// https://www.codewars.com/kata/next-smaller-number-with-the-same-digits
+    /// </summary>
+    public class SmallerNumber
+    {
+        public static long NextSmaller(long n)
+        {
+            var isMatch = false;
+            var listNum = ConvertToList(n);
+            for (var i = 0; i < listNum.Count; i++)
+            {
+                var last = listNum[i];
+                var listChangeNum = listNum.GetRange(0, i);
+                for (var j = i + 1; j < listNum.Count; j++)
+                {
+                    listChangeNum.Add(listNum[j]);
+                    if (listNum[j] == last ||
+                        (listNum[j] < last && listNum[j] > listNum[j - 1])) break;
+                    if (listNum[j] < last) continue;
+                    listNum[j] = last;
+
+                    if (listChangeNum.Count > 0)
+                    {
+                        listChangeNum.Sort();
+                        for (var k = 0; k < listChangeNum.Count; k++)
+                        {
+                            listNum[k] = listChangeNum[k];
+                        }
+                    }
+
+                    isMatch = true;
+                    break;
+                }
+
+                if (isMatch) break;
+            }
+
+            return isMatch && listNum.Last() != 0 ? ConvertToLong(listNum) : -1L;
+        }
+
+        private static List<long> ConvertToList(long num)
+        {
+            var listNum = new List<long>();
+            while (num > 0)
+            {
+                listNum.Add(num % 10);
+                num /= 10;
+            }
+            return listNum;
+        }
+
+        private static long ConvertToLong(List<long> listNum)
+        {
+            return listNum.Select((t, i) => t * (long)Math.Pow(10, i)).Sum();
+        }
+    }
+}
