7b1d6c851ea715cc87b5af294baf29a5479959c8
zhoubo56 <zhoubo56@126.com>
2019-06-17 17:28:41 +0800

    完成4级的Next bigger number with the same digits

---
+++ CodeWarsTrains/Testkyu4/BiggerNumberTest.cs
@@ -0,0 +1,20 @@
+﻿using kyu4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testkyu4
+{
+    [TestClass]
+    public class BiggerNumberTest
+    {
+        [TestMethod]
+        public void TestNextBiggerNumber()
+        {
+            //SampleTests
+            //Assert.AreEqual(21, BiggerNumber.NextBiggerNumber(12));
+            //Assert.AreEqual(531, BiggerNumber.NextBiggerNumber(513));
+            //Assert.AreEqual(2071, BiggerNumber.NextBiggerNumber(2017));
+            Assert.AreEqual(441, BiggerNumber.NextBiggerNumber(414));
+            Assert.AreEqual(414, BiggerNumber.NextBiggerNumber(144));
+        }
+    }
+}
--- CodeWarsTrains/Testkyu4/SmallerNumberTest.cs
+++ CodeWarsTrains/Testkyu4/SmallerNumberTest.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using kyu4;
+﻿using kyu4;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Testkyu4
