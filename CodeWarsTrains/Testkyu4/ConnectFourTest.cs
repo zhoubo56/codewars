aef3deafe293f25c3c4e82d27f84f9324308efb2
zhoubo56 <zhoubo56@126.com>
2019-06-26 09:52:43 +0800

    完成4级的Connect Four

---
+++ CodeWarsTrains/Testkyu4/ConnectFourTest.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using kyu4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testkyu4
+{
+    [TestClass]
+    public class ConnectFourTest
+    {
+        [TestMethod]
+        public void FirstTest()
+        {
+            List<string> myList = new List<string>()
+            {
+                "A_Red",
+                "B_Yellow",
+                "A_Red",
+                "B_Yellow",
+                "A_Red",
+                "B_Yellow",
+                "G_Red",
+                "B_Yellow"
+            };
+            Assert.AreEqual("Yellow", ConnectFour.WhoIsWinner(myList), "it should return Yellow");
+        }
+
+        [TestMethod]
+        public void SecondTest()
+        {
+            List<string> myList = new List<string>()
+            {
+                "C_Yellow",
+                "E_Red",
+                "G_Yellow",
+                "B_Red",
+                "D_Yellow",
+                "B_Red",
+                "B_Yellow",
+                "G_Red",
+                "C_Yellow",
+                "C_Red",
+                "D_Yellow",
+                "F_Red",
+                "E_Yellow",
+                "A_Red",
+                "A_Yellow",
+                "G_Red",
+                "A_Yellow",
+                "F_Red",
+                "F_Yellow",
+                "D_Red",
+                "B_Yellow",
+                "E_Red",
+                "D_Yellow",
+                "A_Red",
+                "G_Yellow",
+                "D_Red",
+                "D_Yellow",
+                "C_Red"
+            };
+            Assert.AreEqual("Yellow", ConnectFour.WhoIsWinner(myList));
+        }
+
+        [TestMethod]
+        public void ThirdTest()
+        {
+            List<string> myList = new List<string>()
+            {
+                "A_Yellow",
+                "B_Red",
+                "B_Yellow",
+                "C_Red",
+                "G_Yellow",
+                "C_Red",
+                "C_Yellow",
+                "D_Red",
+                "G_Yellow",
+                "D_Red",
+                "G_Yellow",
+                "D_Red",
+                "F_Yellow",
+                "E_Red",
+                "D_Yellow"
+            };
+            Assert.AreEqual("Red", ConnectFour.WhoIsWinner(myList), "it should return Red");
+        }
+
+        [TestMethod]
+        public void RandomTest1()
+        {
+            var myList =
+                "C_Yellow-A_Red-D_Yellow-C_Red-E_Yellow-G_Red-A_Yellow-F_Red-F_Yellow-E_Red-C_Yellow-B_Red-A_Yellow-D_Red-E_Yellow-C_Red-B_Yellow-A_Red-C_Yellow-B_Red-A_Yellow-D_Red-D_Yellow-D_Red-D_Yellow-C_Red-A_Yellow-G_Red-F_Yellow-B_Red-F_Yellow-F_Red-E_Yellow-B_Red-B_Yellow-G_Red-E_Yellow-E_Red-F_Yellow-G_Red-G_Yellow-G_Red"
+                    .Split('-').ToList();
+            Assert.AreEqual("Red", ConnectFour.WhoIsWinner(myList), "it should return Red");
+        }
+
+        [TestMethod]
+        public void RandomTest2()
+        {
+            var myList =
+                "F_Yellow-E_Red-A_Yellow-B_Red-D_Yellow-D_Red-G_Yellow-B_Red-B_Yellow-F_Red-E_Yellow-C_Red-E_Yellow-G_Red-A_Yellow-B_Red-A_Yellow-A_Red-B_Yellow-B_Red-D_Yellow-E_Red-E_Yellow-G_Red-E_Yellow-F_Red-D_Yellow-A_Red-C_Yellow-C_Red-D_Yellow-F_Red-G_Yellow-G_Red-D_Yellow-C_Red-F_Yellow-A_Red-F_Yellow-G_Red-C_Yellow-C_Red"
+                    .Split('-').ToList();
+            Assert.AreEqual("Red", ConnectFour.WhoIsWinner(myList), "it should return Red");
+        }
+
+        [TestMethod]
+        public void RandomTest3()
+        {
+            var myList =
+                "B_Yellow-B_Red-D_Yellow-B_Red-B_Yellow-B_Red-E_Yellow-F_Red-D_Yellow-G_Red-A_Yellow-D_Red-B_Yellow-E_Red-A_Yellow-G_Red-G_Yellow-A_Red-C_Yellow-F_Red-A_Yellow-F_Red-G_Yellow-D_Red-G_Yellow-C_Red-E_Yellow-A_Red-A_Yellow-C_Red-C_Yellow-G_Red-E_Yellow-F_Red-D_Yellow-E_Red-E_Yellow-D_Red-C_Yellow-C_Red-F_Yellow-F_Red"
+                    .Split('-').ToList();
+            Assert.AreEqual("Yellow", ConnectFour.WhoIsWinner(myList), "it should return Red");
+        }
+    }
+}
---
+++ CodeWarsTrains/kyu4/ConnectFour.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace kyu4
+{
+    /// <summary>
+    /// Connect Four
+    /// https://www.codewars.com/kata/connect-four-1
+    /// </summary>
+    public class ConnectFour
+    {
+        private const int WinCount = 4;
+        private const int VerticalLength = 6;
+        private const int HorizontalLength = 7;
+        private const int StartNum = 65; //char "A" ascii code
+        private static readonly string[][] Grid = new string[HorizontalLength][];
+
+        public static string WhoIsWinner(List<string> piecesPositionList)
+        {
+            var result = "Draw";
+            for (var i = 0; i < HorizontalLength; i++)
+            {
+                Grid[i] = new string[VerticalLength];
+            }
+
+            foreach (var position in piecesPositionList)
+            {
+                var positions = position.Split('_');
+                var horizontal = GetHorizontal(positions[0]);
+                var vertical = NextStep(horizontal, positions[1]);
+
+                if (!CheckWin(horizontal, vertical, positions[1])) continue;
+                result = positions[1];
+                break;
+            }
+
+            return result;
+        }
+
+        private static int NextStep(int horizontal, string color)
+        {
+            var result = -1;
+            for (var i = 0; i < VerticalLength; i++)
+            {
+                if (!string.IsNullOrEmpty(Grid[horizontal][i])) continue;
+                Grid[horizontal][i] = color;
+                result = i;
+                break;
+            }
+
+            return result;
+        }
+
+        private static bool CheckWin(int horizontal, int vertical, string color)
+        {
+            return
+                (WinCount + 1 <= (MatchCount(horizontal, vertical, 1, 0, color) + MatchCount(horizontal, vertical, -1, 0, color)))
+                || (WinCount + 1 <= (MatchCount(horizontal, vertical, 0, 1, color) + MatchCount(horizontal, vertical, 0, -1, color)))
+                || (WinCount + 1 <= (MatchCount(horizontal, vertical, 1, 1, color) + MatchCount(horizontal, vertical, -1, -1, color)))
+                || (WinCount + 1 <= (MatchCount(horizontal, vertical, 1, -1, color) + MatchCount(horizontal, vertical, -1, 1, color)));
+        }
+
+        private static int MatchCount(int horizontal, int vertical, int x, int y, string color)
+        {
+            if (horizontal < 0 || horizontal >= HorizontalLength || vertical < 0 ||
+                vertical >= VerticalLength) return 0;
+
+            if (color.Equals(Grid[horizontal][vertical]))
+            {
+                return MatchCount(horizontal + x, vertical + y, x, y, color) + 1;
+            }
+
+            return 0;
+        }
+
+        private static int GetHorizontal(string str)
+        {
+            var array = Encoding.ASCII.GetBytes(str);
+            var ascii = (int)(array[0]);
+            return ascii - StartNum;
+        }
+    }
+}
