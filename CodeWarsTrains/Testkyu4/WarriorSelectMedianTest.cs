ff2042423b0b151e8a2daaf87700f7b02f920aa5
zhoubo56 <zhoubo56@126.com>
2019-07-29 14:20:23 +0800

    添加4级的Select median(未完成)

---
+++ CodeWarsTrains/Testkyu4/WarriorSelectMedianTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+using Common;
+using kyu4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testkyu4
+{
+    /// <summary>
+    /// Summary description for SelectMedianTest
+    /// </summary>
+    [TestClass]
+    public class WarriorSelectMedianTest
+    {
+        [TestMethod]
+        public void SimpleTest()
+        {
+            //try
+            //{
+                var input = new Warrior[]{
+                    new Warrior(1),
+                    new Warrior(2),
+                    new Warrior(3),
+                    new Warrior(4),
+                    new Warrior(5),
+                  };
+                Warrior.ResetCompareCount();
+                Assert.AreSame(input[3], WarriorSelectMedian.SelectMedian(input));
+                Assert.AreEqual(true, Warrior.CompareCount <= 6);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Assert.Fail(ex.Message);
+            //}
+        }
+
+        [TestMethod]
+        public void CombinationCombinationTest()
+        {
+
+            var warriors = new Warrior[]{
+                    new Warrior(1),
+                    new Warrior(4),
+                    new Warrior(5),
+                    new Warrior(3),
+                    new Warrior(2),
+                };
+            var listCombination = PermutationAndCombination<Warrior>.GetPermutation(warriors);
+
+            foreach (var combination in listCombination)
+            {
+                try
+                {
+                    //Console.WriteLine(string.Join("-", combination.Select(c => c.m_internal.ToString())));
+                    Warrior.ResetCompareCount();
+                    var result = WarriorSelectMedian.SelectMedian(combination);
+                    //Console.WriteLine(Warrior.CompareCount);
+                    Assert.AreSame(warriors[3], result);
+                    Assert.AreEqual(true, Warrior.CompareCount <= 6);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message + string.Join("-", combination.Select(c => c.m_internal.ToString())));
+                    //Assert.Fail(ex.Message);
+                }
+            }
+        }
+    }
+
+    public class Warrior : IWarrior
+    {
+        public int m_internal { get; set; }
+
+        public static int CompareCount { get; private set; }
+
+        public Warrior(int rank = 0)
+        {
+            m_internal = rank;
+        }
+
+        public bool IsBetter(IWarrior other)
+        {
+            ++CompareCount;
+            if (other == null || !(other is Warrior)) return false;
+            return m_internal >= (other as Warrior).m_internal;
+        }
+
+        public static void ResetCompareCount()
+        {
+            CompareCount = 0;
+        }
+    }
+}
---
+++ CodeWarsTrains/kyu4/WarriorSelectMedian.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace kyu4
+{
+    /// <summary>
+    /// Select median
+    /// https://www.codewars.com/kata/select-median
+    /// </summary>
+    public class WarriorSelectMedian
+    {
+        public static IWarrior SelectMedian(IWarrior[] warriors)
+        {
+            IWarrior smaller1, smaller2, bigger1, bigger2;
+            if (warriors[0].IsBetter(warriors[1]))
+            {
+                smaller1 = warriors[1];
+                bigger1 = warriors[0];
+            }
+            else
+            {
+                smaller1 = warriors[0];
+                bigger1 = warriors[1];
+            }
+
+            if (warriors[3].IsBetter(warriors[4]))
+            {
+                smaller2 = warriors[4];
+                bigger2 = warriors[3];
+            }
+            else
+            {
+                smaller2 = warriors[3];
+                bigger2 = warriors[4];
+            }
+
+            if (warriors[2].IsBetter(smaller1))
+            {
+                if (warriors[2].IsBetter(smaller2))
+                {
+                    Console.WriteLine("pass 1");
+                    //if (bigger1.IsBetter(bigger2))
+                    //{
+                    //    return warriors[2].IsBetter(bigger2) ? bigger2.IsBetter(smaller1) ? bigger2 : smaller1 : warriors[2];
+                    //}
+                    //else
+                    //{
+                    //    return warriors[2].IsBetter(bigger1) ? bigger1.IsBetter(smaller2) ? bigger1 : smaller2 : warriors[2];
+                    //}
+                    if (smaller1.IsBetter(bigger2))
+                    {
+                        //s2 b2 s1 w2 b1
+                        return smaller1;
+                    }
+                    else if (smaller2.IsBetter(bigger1))
+                    {
+                        return smaller2;
+                    }
+                    else
+                    {
+                        return warriors[2];
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("pass 2");
+                    if (bigger1.IsBetter(smaller2))
+                    {
+                        return warriors[2].IsBetter(smaller2) ? warriors[2] : smaller2;
+                    }
+                    else
+                    {
+                        return warriors[2].IsBetter(bigger1) ? warriors[2] : bigger1;
+                    }
+                }
+            }
+            else
+            {
+                if (warriors[2].IsBetter(smaller2))
+                {
+                    Console.WriteLine("pass 3");
+                    if (bigger2.IsBetter(smaller1))
+                    {
+                        return warriors[2].IsBetter(smaller1) ? warriors[2] : smaller1;
+                    }
+                    else
+                    {
+                        return warriors[2].IsBetter(bigger2) ? warriors[2] : bigger2;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("pass 4");
+                    if (smaller1.IsBetter(smaller2))
+                    {
+                        return bigger2.IsBetter(smaller1) ? smaller1 : bigger2;
+                    }
+                    else
+                    {
+                        return bigger1.IsBetter(smaller2) ? smaller2 : bigger1;
+                    }
+                }
+            }
+        }
+    }
+
+    public interface IWarrior
+    {
+        // a.IsBetter(b) returns true if and only if
+        // warrior a is no worse than warrior b, i.e. a>=b
+        bool IsBetter(IWarrior other);
+    }
+}
