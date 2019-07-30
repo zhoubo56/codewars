eed55023e79dab08ba8667b2e3c1f87962a93ad1
zhoubo56 <zhoubo56@126.com>
2019-07-30 10:49:10 +0800

    完成5级的John and Ann sign up for Codewars

---
+++ CodeWarsTrains/Testkyu5/JohannTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using kyu5;
+
+namespace Testkyu5
+{
+    /// <summary>
+    /// Summary description for JohannTest
+    /// </summary>
+    [TestClass]
+    public class JohannTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Console.WriteLine("Basic Tests John");
+            TestJohn(11, "[0, 0, 1, 2, 2, 3, 4, 4, 5, 6, 6]");
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            Console.WriteLine("Basic Tests Ann");
+            TestAnn(6, "[1, 1, 2, 2, 3, 3]");
+        } 
+
+        [TestMethod]
+        public void Test3()
+        {
+            Console.WriteLine("Basic Tests SumAnn");
+            TestSumAnn(115, 4070);
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            Console.WriteLine("Basic Tests SumJohn");
+            TestSumJohn(75, 1720);
+        }
+
+        private string Array2String(List<long> list)
+        {
+            return "[" + string.Join(", ", list) + "]";
+        }
+
+        private void TestJohn(long n, string res)
+        {
+            Assert.AreEqual(res, Array2String(Johnann.John(n)));
+        }
+
+        private void TestAnn(long n, string res)
+        {
+            Assert.AreEqual(res, Array2String(Johnann.Ann(n)));
+        }
+
+        private void TestSumAnn(long n, long res)
+        {
+            Assert.AreEqual(res, Johnann.SumAnn(n));
+        }
+
+        private void TestSumJohn(long n, long res)
+        {
+            Assert.AreEqual(res, Johnann.SumJohn(n));
+        }
+    }
+}
---
+++ CodeWarsTrains/kyu5/Johnann.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace kyu5
+{
+    /// <summary>
+    /// John and Ann sign up for Codewars
+    /// https://www.codewars.com/kata/john-and-ann-sign-up-for-codewars
+    /// </summary>
+    public class Johnann
+    {
+        public static List<long> John(long n)
+        {
+            return GetNamedList("John", n);
+        }
+
+        public static List<long> Ann(long n)
+        {
+            return GetNamedList("Ann", n);
+        }
+
+        public static long SumJohn(long n)
+        {
+            return GetNamedList("John", n).Aggregate(0L, (current, t) => current + t);
+        }
+
+        public static long SumAnn(long n)
+        {
+            return GetNamedList("Ann", n).Sum();
+        }
+
+        private static List<long> GetNamedList(string name, long n)
+        {
+            var johnList = new List<long>() { 0, 0 };
+            var annList = new List<long>() { 1, 1 };
+            for (var i = 2; i < n; i++)
+            {
+                johnList.Add(i - annList[(int)johnList[i - 1]]);
+                annList.Add(i - johnList[(int)annList[i - 1]]);
+            }
+            switch (name)
+            {
+                case "Ann":
+                    return annList;
+                case "John":
+                    return johnList;
+                default:
+                    return new List<long>();
+            }
+        }
+    }
+}
