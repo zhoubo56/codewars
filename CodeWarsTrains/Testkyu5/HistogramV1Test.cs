ebf9e570870cb7796e1b324717c24077bbc4aa58
zhoubo56 <zhoubo56@126.com>
2019-08-23 16:23:55 +0800

    完成5级的Histogram - V1

---
+++ CodeWarsTrains/Testkyu5/HistogramV1Test.cs
@@ -0,0 +1,60 @@
+﻿using kyu5;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testkyu5
+{
+    [TestClass]
+    public class HistogramV1Test
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+            string expected =
+                "    10\n" +
+                "    #\n" +
+                "    #\n" +
+                "7   #\n" +
+                "#   #\n" +
+                "#   #     5\n" +
+                "#   #     #\n" +
+                "# 3 #     #\n" +
+                "# # #     #\n" +
+                "# # # 1   #\n" +
+                "# # # #   #\n" +
+                "-----------\n" +
+                "1 2 3 4 5 6\n";
+            Assert.AreEqual(expected, HistogramV1.Histogram(new int[] { 7, 3, 10, 1, 0, 5 }));
+        }
+
+        [TestMethod]
+        public void ZeroTest()
+        {
+            string expected =
+                "-----------\n" +
+                "1 2 3 4 5 6\n";
+            
+            Assert.AreEqual(expected, HistogramV1.Histogram(new int[] { 0, 0, 0, 0, 0, 0 }));
+        }
+
+        [TestMethod]
+        public void RandomTest()
+        {
+            string expected =
+                "    11    11\n" +
+                "  10#     #\n" +
+                "  # #     #\n" +
+                "  # # 8   #\n" +
+                "  # # #   #\n" +
+                "  # # #   #\n" +
+                "5 # # # 5 #\n" +
+                "# # # # # #\n" +
+                "# # # # # #\n" +
+                "# # # # # #\n" +
+                "# # # # # #\n" +
+                "# # # # # #\n" +
+                "-----------\n" +
+                "1 2 3 4 5 6\n";
+            Assert.AreEqual(expected, HistogramV1.Histogram(new int[] { 5, 10, 11, 8, 5, 11 }));
+        }
+    }
+}
---
+++ CodeWarsTrains/kyu5/HistogramV1.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+
+namespace kyu5
+{
+    /// <summary>
+    /// Histogram - V1
+    /// https://www.codewars.com/kata/histogram-v1
+    /// </summary>
+    public class HistogramV1
+    {
+        public static string Histogram(int[] results)
+        {
+            var strHistogram = string.Empty;
+            var height = results.Max() + 1;
+            if (height > 1)
+            {
+                var width = results.Length * 2;
+                var histogram = new string[width, height];
+                for (var i = 0; i < width; i++)
+                {
+                    for (var j = 0; j <= results[i / 2]; j++)
+                    {
+                        if (i % 2 == 1)
+                        {
+                            if (j == results[i / 2] && j > 9)
+                            {
+                                histogram[i, j] = "";
+                            }
+                            else
+                            {
+                                histogram[i, j] = " ";
+                            }
+                        }
+                        else
+                        {
+                            if (j == results[i / 2])
+                            {
+                                histogram[i, j] = j == 0 ? " " : j.ToString();
+                            }
+                            else
+                            {
+                                histogram[i, j] = "#";
+                            }
+                        }
+                    }
+                }
+
+                for (var i = height - 1; i >= 0; i--)
+                {
+                    for (var j = 0; j < width; j++)
+                    {
+                        strHistogram += histogram[j, i] == null ? " " : histogram[j, i];
+                    }
+
+                    strHistogram = strHistogram.TrimEnd() + "\n";
+                }
+            }
+
+            strHistogram += string.Join("-", results.Select(r => "-").ToArray()) + "\n";
+            strHistogram += string.Join(" ", results.Select((r, i) => i + 1).ToArray()) + "\n";
+            return strHistogram;
+        }
+    }
+}
