6b62c6119b5612120ca3592386883c789290c2e9
zhoubo56 <zhoubo56@126.com>
2019-09-17 11:41:17 +0800

    完成3级的Rail Fence Cipher: Encoding and Decoding

---
+++ CodeWarsTrains/Testkyu3/RailFenceCipherTest.cs
@@ -0,0 +1,72 @@
+﻿using kyu3;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testkyu3
+{
+    /// <summary>
+    /// Summary description for RailFenceCipherEncodingAndDecodingTest
+    /// </summary>
+    [TestClass]
+    public class RailFenceCipherTest
+    {
+        [TestMethod]
+        public void EncodeSampleTests()
+        {
+            string[][] encodes =
+            {
+                new[] { "WEAREDISCOVEREDFLEEATONCE", "WECRLTEERDSOEEFEAOCAIVDEN" },  // 3 rails
+                new[] { "Hello, World!", "Hoo!el,Wrdl l" },    // 3 rails
+                new[] { "Hello, World!", "H !e,Wdloollr" },    // 4 rails
+                new[] { "", "" },                               // 3 rails (even if...)
+                new[] { "123456789", "159246837" }
+            };
+            int[] rails = { 3, 3, 4, 3, 3 };
+            for (int i = 0; i < encodes.Length; i++)
+            {
+                Assert.AreEqual(encodes[i][1], RailFenceCipher.Encode(encodes[i][0], rails[i]));
+            }
+        }
+
+        [TestMethod]
+        public void DecodeSampleTests()
+        {
+            string[][] decodes =
+            {
+                new[] { "159246837", "123456789" },
+                new[] { "WECRLTEERDSOEEFEAOCAIVDEN", "WEAREDISCOVEREDFLEEATONCE" },    // 3 rails
+                new[] { "H !e,Wdloollr", "Hello, World!" },    // 4 rails
+                new[] { "", "" }                               // 3 rails (even if...)
+            };
+            int[] rails = { 3, 3, 4, 3 };
+            for (int i = 0; i < decodes.Length; i++)
+            {
+                Assert.AreEqual(decodes[i][1], RailFenceCipher.Decode(decodes[i][0], rails[i]));
+            }
+        }
+
+        [TestMethod]
+        public void PublisherIdentityPermission()
+        {
+            //string[][] encodes =
+            //{
+            //    new[] { "WEAREDISCOVEREDFLEEATONCE", "WEEEAALTRFOEDNDECIRESECVO" },
+            //    new[] { "WADCEFACTLROIREESVEEOENED", "WEAREDISCOVEREDFLEEATONCE" }
+            //};
+            //int[] rails1 = { 10, 8 };
+            //for (int i = 0; i < encodes.Length; i++)
+            //{
+            //    Assert.AreEqual(encodes[i][1], RailFenceCipher.Encode(encodes[i][0], rails1[i]));
+            //}
+
+            string[][] decodes =
+            {
+                new[] { "WEAREDISCOVEREDFLEEATONCE", "WADCEFACTLROIREESVEEOENED" },
+            };
+            int[] rails2 = { 8 };
+            for (int i = 0; i < decodes.Length; i++)
+            {
+                Assert.AreEqual(decodes[i][1], RailFenceCipher.Decode(decodes[i][0], rails2[i]));
+            }
+        }
+    }
+}
---
+++ CodeWarsTrains/kyu3/RailFenceCipher.cs
@@ -0,0 +1,60 @@
+﻿namespace kyu3
+{
+    /// <summary>
+    /// Rail Fence Cipher: Encoding and Decoding
+    /// https://www.codewars.com/kata/rail-fence-cipher-encoding-and-decoding
+    /// </summary>
+    public class RailFenceCipher
+    {
+        public static string Encode(string s, int n)
+        {
+            var strArray = new string[n];
+            var flag = 0;
+            var option = true;
+            foreach (var c in s)
+            {
+                strArray[flag] += c;
+                if (option)
+                {
+                    flag++;
+                    if (flag == n - 1) option = false;
+                }
+                else
+                {
+                    flag--;
+                    if (flag == 0) option = true;
+                }
+            }
+            return string.Join("", strArray);
+        }
+
+        public static string Decode(string s, int n)
+        {
+            var charArray = new char[s.Length];
+            var index = 0;
+            var line = 1;
+            var option = true;
+            foreach (var c in s)
+            {
+                charArray[index] = c;
+                if (option)
+                {
+                    index += (n - line) * 2;
+                }
+                else
+                {
+                    index += (line - 1) * 2;
+                }
+
+                if (line != 1 && line != n) option = !option;
+
+                if (index < s.Length) continue;
+                index = line;
+                line++;
+                option = line != n;
+            }
+
+            return string.Join("", charArray);
+        }
+    }
+}
