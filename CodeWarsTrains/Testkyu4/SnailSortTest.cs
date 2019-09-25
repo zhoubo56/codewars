c76feb360bc40e1261f79ecdcedda6edb31d9608
zhoubo56 <zhoubo56@126.com>
2019-09-25 17:40:09 +0800

    完成4级的Snail

---
+++ CodeWarsTrains/Testkyu4/SnailSortTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using kyu4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testkyu4
+{
+    /// <summary>
+    /// Summary description for SnailSortTest
+    /// </summary>
+    [TestClass]
+    public class SnailSortTest
+    {
+        [TestMethod]
+        public void SnailTest1()
+        {
+            int[][] array =
+            {
+                new []{1, 2, 3},
+                new []{4, 5, 6},
+                new []{7, 8, 9}
+            };
+            var r = new[] { 1, 2, 3, 6, 9, 8, 7, 4, 5 };
+            Test(array, r);
+        }
+
+        [TestMethod]
+        public void SnailTest2()
+        {
+            int[][] array =
+            {
+                new []{1, 2, 3, 4},
+                new []{5, 6, 7, 8},
+                new []{9, 10, 11, 12},
+                new []{13, 14, 15, 16}
+            };
+            var r = new[] { 1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10 };
+            Test(array, r);
+        }
+
+        public string Int2DToString(int[][] a)
+        {
+            return $"[{string.Join("\n", a.Select(row => $"[{string.Join(",", row)}]"))}]";
+        }
+
+        public void Test(int[][] array, int[] result)
+        {
+            var text = $"{Int2DToString(array)}\nshould be sorted to\n[{string.Join(",", result)}]\n";
+            Console.WriteLine(text);
+            CollectionAssert.AreEqual(result, SnailSort.Snail(array));
+        }
+    }
+}
---
+++ CodeWarsTrains/kyu4/SnailSort.cs
@@ -0,0 +1,57 @@
+﻿namespace kyu4
+{
+    /// <summary>
+    /// Snail
+    /// https://www.codewars.com/kata/snail
+    /// </summary>
+    public class SnailSort
+    {
+        public static int[] Snail(int[][] array)
+        {
+            var totalCount = array.Length * array[0].Length;
+            var result = new int[totalCount];
+            var x = 0;
+            var xOption = true;
+            var y = 0;
+            var yOption = true;
+            var flag = 'x';
+            for (var i = 0; i < totalCount; i++)
+            {
+                switch (flag)
+                {
+                    case 'x':
+                        {
+                            result[i] = array[x][y];
+                            if (x + y == array[x].Length - 1)
+                            {
+                                x = yOption ? x + 1 : x - 1;
+                                flag = 'y';
+                                xOption = !xOption;
+                                continue;
+                            }
+
+                            y = xOption ? y + 1 : y - 1;
+                            break;
+                        }
+                    case 'y':
+                        {
+                            result[i] = array[x][y];
+                            if ((yOption && x == y)
+                                || (!yOption && x == y + 1))
+                            {
+                                y = xOption ? y + 1 : y - 1;
+                                flag = 'x';
+                                yOption = !yOption;
+                                continue;
+                            }
+
+                            x = yOption ? x + 1 : x - 1;
+                            break;
+                        }
+                }
+            }
+
+            return result;
+        }
+    }
+}
