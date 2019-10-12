b692fd122271c94a9dc87ba5dd8486653535ccf9
zhoubo56 <zhoubo56@126.com>
2019-10-12 17:17:53 +0800

    完成3级的The Lift，代码是面向对象版本，性能还不达标

---
+++ CodeWarsTrains/Testkyu3/TheLiftTest.cs
@@ -0,0 +1,77 @@
+﻿using kyu3;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testkyu3
+{
+    [TestClass]
+    public class TheLiftTest
+    {
+        [TestMethod]
+        public void TestUp()
+        {
+            int[][] queues =
+            {
+                new int[0], // G
+                new int[0], // 1
+                new int[]{5,5,5}, // 2
+                new int[0], // 3
+                new int[0], // 4
+                new int[0], // 5
+                new int[0], // 6
+            };
+            var result = TheLiftClass.TheLift(queues, 5);
+            CollectionAssert.AreEqual(new[] { 0, 2, 5, 0 }, result);
+        }
+
+        [TestMethod]
+        public void TestDown()
+        {
+            int[][] queues =
+            {
+                new int[0], // G
+                new int[0], // 1
+                new int[]{1,1}, // 2
+                new int[0], // 3
+                new int[0], // 4
+                new int[0], // 5
+                new int[0], // 6
+            };
+            var result = TheLiftClass.TheLift(queues, 5);
+            CollectionAssert.AreEqual(new[] { 0, 2, 1, 0 }, result);
+        }
+
+        [TestMethod]
+        public void TestUpAndUp()
+        {
+            int[][] queues =
+            {
+                new int[0], // G
+                new int[]{3}, // 1
+                new int[]{4}, // 2
+                new int[0], // 3
+                new int[]{5}, // 4
+                new int[0], // 5
+                new int[0], // 6
+            };
+            var result = TheLiftClass.TheLift(queues, 5);
+            CollectionAssert.AreEqual(new[] { 0, 1, 2, 3, 4, 5, 0 }, result);
+        }
+
+        [TestMethod]
+        public void TestDownAndDown()
+        {
+            int[][] queues =
+            {
+                new int[0], // G
+                new int[]{0}, // 1
+                new int[0], // 2
+                new int[0], // 3
+                new int[]{2}, // 4
+                new int[]{3}, // 5
+                new int[0], // 6
+            };
+            var result = TheLiftClass.TheLift(queues, 5);
+            CollectionAssert.AreEqual(new[] { 0, 5, 4, 3, 2, 1, 0 }, result);
+        }
+    }
+}
---
+++ CodeWarsTrains/kyu3/TheLift.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace kyu3
+{
+    /// <summary>
+    /// The Lift
+    /// https://www.codewars.com/kata/the-lift
+    /// </summary>
+    public class TheLiftClass
+    {
+        public static int[] TheLift(int[][] queues, int capacity)
+        {
+            var stoppedFloors = new List<int>() { 0 };
+            var lift = new Lift(capacity, queues.Length - 1);
+            for (var i = 0; i < queues.Length; i++)
+            {
+                if (queues[i].Length <= 0) continue;
+                foreach (var i1 in queues[i])
+                {
+                    lift.EntryWaitingQueues(new People(i, i1));
+                }
+            }
+            lift.Run(ref stoppedFloors);
+            return stoppedFloors.ToArray();
+        }
+    }
+
+    /// <summary>
+    /// 电梯
+    /// </summary>
+    public class Lift
+    {
+        /// <summary>
+        /// 容量
+        /// </summary>
+        private readonly int _capacity;
+        /// <summary>
+        /// 楼层数
+        /// </summary>
+        private readonly int _floorNumber;
+        /// <summary>
+        /// 乘坐电梯的人
+        /// </summary>
+        private readonly List<People> _ridingPeoples = new List<People>();
+        /// <summary>
+        /// 等待电梯的人
+        /// </summary>
+        private readonly List<People> _waitingPeoples = new List<People>();
+
+        public Lift(int capacity, int floorNumber)
+        {
+            _capacity = capacity;
+            _floorNumber = floorNumber;
+            Direction = -1;
+            CurrentFloor = 0;
+        }
+
+        /// <summary>
+        /// 方向
+        /// 1:向上
+        /// -1:向下
+        /// </summary>
+        public int Direction { get; private set; }
+
+        /// <summary>
+        /// 当前楼层
+        /// </summary>
+        public int CurrentFloor { get; private set; }
+
+        private void ReverseDirection()
+        {
+            Direction *= -1;
+        }
+
+        private bool ExitArrivedPeople()
+        {
+            return _ridingPeoples.RemoveAll(p => p.WannaFloor == CurrentFloor) > 0;
+        }
+
+        private bool EntryOnePeople(People people)
+        {
+            if (_ridingPeoples.Count >= _capacity)
+            {
+                return false;
+            }
+            else
+            {
+                _ridingPeoples.Add(people);
+                return true;
+            }
+        }
+
+        private void GotoNextFloor()
+        {
+            if (CurrentFloor == _floorNumber || CurrentFloor == 0)
+            {
+                ReverseDirection();
+            }
+            CurrentFloor += Direction;
+        }
+
+        /// <summary>
+        /// 进入等待队列
+        /// </summary>
+        /// <param name="people"></param>
+        public void EntryWaitingQueues(People people)
+        {
+            _waitingPeoples.Add(people);
+        }
+
+        /// <summary>
+        /// 运行
+        /// </summary>
+        /// <param name="stoppedFloors"></param>
+        public void Run(ref List<int> stoppedFloors)
+        {
+            //有等待的人或者有乘坐的人
+            while (_waitingPeoples.Count > 0 || _ridingPeoples.Count > 0)
+            {
+                var needStop = ExitArrivedPeople();
+                foreach (var currentFloorWaitingPeople in _waitingPeoples.Where(p => p.StandingFloor == CurrentFloor).ToList())
+                {
+                    if (Direction != currentFloorWaitingPeople.Direction) continue;
+                    if (!EntryOnePeople(currentFloorWaitingPeople)) break;
+                    needStop = true;
+                    _waitingPeoples.Remove(currentFloorWaitingPeople);
+                }
+                if (needStop) stoppedFloors.Add(CurrentFloor);
+                GotoNextFloor();
+            }
+
+            //最终回到ground层
+            if (stoppedFloors.Last() != 0) stoppedFloors.Add(0);
+        }
+    }
+
+    /// <summary>
+    /// 坐电梯的人
+    /// </summary>
+    public class People
+    {
+        public People(int standingFloor, int wannaFloor)
+        {
+            StandingFloor = standingFloor;
+            WannaFloor = wannaFloor;
+            Direction = standingFloor > wannaFloor ? -1 : 1;
+        }
+        
+        /// <summary>
+        /// 站立的楼层
+        /// </summary>
+        public int StandingFloor { get; private set; }
+
+        /// <summary>
+        /// 想去的楼层
+        /// </summary>
+        public int WannaFloor { get; private set; }
+
+        /// <summary>
+        /// 方向
+        /// 1:向上
+        /// -1:向下
+        /// </summary>
+        public int Direction { get; private set; }
+    }
+}
