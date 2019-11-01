94b08af6351a3864ea9acd92b85d8211429343df
zhoubo56 <zhoubo56@126.com>
2019-11-01 13:36:46 +0800

    完成4级的Strip Comments

---
+++ CodeWarsTrains/Testkyu4/StripCommentsTest.cs
@@ -0,0 +1,20 @@
+﻿using kyu4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testkyu4
+{
+    [TestClass]
+    public class StripCommentsTest
+    {
+        [TestMethod]
+        public void StripComments()
+        {
+            Assert.AreEqual(
+                "apples, pears\ngrapes\nbananas",
+                StripCommentsSolution.StripComments("apples, pears # and bananas\ngrapes\nbananas !apples", new string[] { "#", "!" }));
+
+            Assert.AreEqual("a\nc\nd", StripCommentsSolution.StripComments("a #b\nc\nd $e f g", new string[] { "#", "$" }));
+
+        }
+    }
+}
---
+++ CodeWarsTrains/kyu4/StripCommentsSolution.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace kyu4
+{
+    /// <summary>
+    /// Strip Comments
+    /// https://www.codewars.com/kata/strip-comments
+    /// </summary>
+    public class StripCommentsSolution
+    {
+        public static string StripComments(string text, string[] commentSymbols)
+        {
+            text = commentSymbols.Aggregate(text, (current, commentSymbol) => current.Replace(commentSymbol, "#"));
+            return string.Join("\n", text.Split('\n').Select(t => t.Split('#').First().TrimEnd()));
+        }
+    }
+}
