515024ec059b87e7b8b08916a52e3cd03616a9ca
zhoubo56 <zhoubo56@126.com>
2019-11-07 15:41:28 +0800

    JS->完成7级的Alphabetical Addition

---
+++ JavaScript/Testkyu7/AlphabeticalAdditionTest.js
@@ -0,0 +1,20 @@
+var a = require('../kyu7/AlphabeticalAddition');
+const { assert } = require('chai');
+
+describe("Fixed tests", () => {
+  const tests = [
+    [['a', 'b', 'c'], 'f'],
+    [['z'], 'z'],
+    [['a', 'b'], 'c'],
+    [['c'], 'c'],
+    [['z', 'a'], 'a'],
+    [['y', 'c', 'b'], 'd'],
+    [[], 'z']
+  ];
+  tests.forEach(test => {
+    const str = test[0].map(x => `"${x}"`).join(', ');
+    it(`addLetters(${str})`, () => {
+      assert.strictEqual(a.addLetters(...test[0]), test[1]);
+    });
+  });
+});
---
+++ JavaScript/kyu7/AlphabeticalAddition.js
@@ -0,0 +1,18 @@
+var startCharASCII = 96;
+var letterCount = 26;
+
+/**
+ * Alphabetical Addition
+ * https://www.codewars.com/kata/alphabetical-addition
+ */
+function addLetters(...letters) {
+    var letterASCII = letters.reduce((p, c, i) => {
+        return p + c.charCodeAt() - startCharASCII;
+    }, 0) % letterCount;
+    if (letterASCII == 0) letterASCII = letterCount;
+    return String.fromCharCode(letterASCII + startCharASCII);
+}
+
+module.exports = {
+    addLetters
+}
