be7c5a94326e8c51c33b1c57f9ae177e5a4a63e2
zhoubo56 <zhoubo56@126.com>
2019-11-08 16:37:06 +0800

    JS->完成6级的Reversing a Process

---
+++ JavaScript/Testkyu6/ReversingProcessTest.js
@@ -0,0 +1,17 @@
+const { decode } = require('../kyu6/ReversingProcess');
+const { assert } = require('chai');
+
+describe("decode", function () {
+
+    function testing_decode(r, expected) {
+        let actual = decode(r);
+        assert.strictEqual(actual, expected);
+    }
+
+    it("Basic tests", function () {
+        testing_decode("1273409kuqhkoynvvknsdwljantzkpnmfgf", "uogbucwnddunktsjfanzlurnyxmx")
+        testing_decode("1544749cdcizljymhdmvvypyjamowl", "mfmwhbpoudfujjozopaugcb")
+        testing_decode("105860ymmgegeeiwaigsqkcaeguicc", "Impossible to decode")
+        testing_decode("1122305vvkhrrcsyfkvejxjfvafzwpsdqgp", "rrsxppowmjsrclfljrajtybwviqb")
+    })
+})
---
+++ JavaScript/kyu6/ReversingProcess.js
@@ -0,0 +1,22 @@
+const startCharASCII = 97;
+const letterCount = 26;
+
+/**
+ * Reversing a Process
+ * https://www.codewars.com/kata/reversing-a-process
+ */
+function decode(r) {
+    var num = parseInt(r);
+    var letters = [];
+    var codes = [];
+    for (var i = 0; i < letterCount; i++) {
+        letters[i] = String.fromCharCode(startCharASCII + i);
+        codes[i] = String.fromCharCode(i * num % letterCount + startCharASCII);
+    }
+    if (Array.from(new Set(codes)).length != letterCount) return 'Impossible to decode';
+    return r.split(num)[1].split('').map((v, i, a) => letters[codes.indexOf(v)]).join('');
+}
+
+module.exports = {
+    decode
+}
