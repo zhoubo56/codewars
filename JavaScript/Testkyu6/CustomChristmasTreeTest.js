92d18183e6994dacc08e1ec2f9278cccd87b1e8e
zhoubo56 <zhoubo56@126.com>
2020-08-10 17:14:37 +0800

    JS->完成6级的Custom Christmas Tree

---
+++ JavaScript/Testkyu6/CustomChristmasTreeTest.js
@@ -0,0 +1,58 @@
+const { customChristmasTree } = require('../kyu6/CustomChristmasTree');
+const { assert } = require('chai');
+
+describe("Basic Tests", function () {
+    it("It should works for basic tests.", function () {
+
+        assert.strictEqual(customChristmasTree("*@o", 3),
+            `  *
+ @ o
+* @ o
+  |`)
+
+        assert.strictEqual(customChristmasTree("*@o", 6),
+            `     *
+    @ o
+   * @ o
+  * @ o *
+ @ o * @ o
+* @ o * @ o
+     |
+     |`)
+
+        assert.strictEqual(customChristmasTree("1234", 6),
+            `     1
+    2 3
+   4 1 2
+  3 4 1 2
+ 3 4 1 2 3
+4 1 2 3 4 1
+     |
+     |`)
+
+        assert.strictEqual(customChristmasTree("123456789", 3),
+            `  1
+ 2 3
+4 5 6
+  |`)
+
+
+
+        assert.strictEqual(customChristmasTree("(|@", 10),
+            `         (
+        | @
+       ( | @
+      ( | @ (
+     | @ ( | @
+    ( | @ ( | @
+   ( | @ ( | @ (
+  | @ ( | @ ( | @
+ ( | @ ( | @ ( | @
+( | @ ( | @ ( | @ (
+         |
+         |
+         |`)
+
+    })
+
+})
---
+++ JavaScript/kyu6/CustomChristmasTree.js
@@ -0,0 +1,29 @@
+const treeUnit = 3;
+/**
+ * Custom Christmas Tree
+ * https://www.codewars.com/kata/5a405ba4e1ce0e1d7800012e
+ * @param {*} chars the specified characters
+ * @param {*} n the specified height. A positive integer greater than 2
+ * @return {string} the tree
+ */
+function customChristmasTree(chars, n) {
+    let strArray = [];
+    let charIndex = 0;
+    for (let i = 0; i < n; i++) {
+        let tmp = new Array(n - i).join(' ');
+        for (let j = i; j >= 0; j--) {
+            tmp += chars[charIndex++ % chars.length] + ' ';
+        }
+        strArray.push(tmp.substring(0, tmp.length - 1));
+    }
+    let index = n;
+    while (index >= 3) {
+        strArray.push(new Array(n).join(' ') + '|');
+        index -= 3;
+    }
+    return strArray.join('\n');
+}
+
+module.exports = {
+    customChristmasTree
+}
