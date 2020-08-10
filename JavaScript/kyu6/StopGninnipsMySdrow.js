5b018867a9b749b71af562939329d628d63dd8c8
zhoubo56 <zhoubo56@126.com>
2020-08-10 17:31:16 +0800

    JS->完成6级的Stop gninnipS My sdroW!

---
+++ JavaScript/kyu6/StopGninnipsMySdrow.js
@@ -0,0 +1,14 @@
+
+/**
+ * Stop gninnipS My sdroW!
+ * https://www.codewars.com/kata/5264d2b162488dc400000001/
+ * @param {string} s 
+ * @return {string} result
+ */
+function spinWords(s) {
+    return s.split(' ').map((v, i, a) => v.length >= 5 ? v.split('').reverse().join('') : v).join(' ');
+}
+
+module.exports = {
+    spinWords
+}
