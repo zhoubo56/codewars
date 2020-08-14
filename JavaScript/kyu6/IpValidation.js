c72a2d7055830dc1151e3f85fded1a378e35e2e2
zhoubo56 <zhoubo56@126.com>
2020-08-14 17:16:30 +0800

    JS->完成6级的IP Validation

---
+++ JavaScript/kyu6/IpValidation.js
@@ -0,0 +1,30 @@
+
+/**
+ * IP Validation
+ * https://www.codewars.com/kata/515decfd9dcfc23bb6000006
+ * @param {string} str guaranteed to be a single string
+ * @return {boolean} is IPV4
+ */
+function isValidIP(str) {
+    console.log(str);
+    var strArr = str.split('.');
+    if (strArr.length != 4) return false;
+    for (const s of strArr) {
+        if (isNaN(s)) return false;
+        if (s.length > 3 || s.length == 0) return false;
+        let n = parseInt(s);
+        if (s.length == 1 && (n < 0 || n > 9)) return false;
+        if (s.length == 2 && (n < 10 || n > 99)) return false;
+        if (s.length == 3 && (n < 100 || n > 255)) return false;
+    }
+
+    return true;
+}
+
+function isValidIP_clever(str) {
+    return str.split('.').filter(function(v){return v==Number(v).toString() && Number(v)<256}).length==4;
+  }
+
+module.exports = {
+    isValidIP
+}
